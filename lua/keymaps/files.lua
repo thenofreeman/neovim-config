49ed2a4aebceec076791e3b96f7fba62875ee5da
nolan freeman abbott <nolan@freemanabbott.com>
2024-10-22 17:35:58 -0500

    add keymap for <L>fs

--- lua/keymaps/files.lua
+++ lua/keymaps/files.lua
@@ -6,5 +6,6 @@ local builtin = require 'telescope.builtin'
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find file' })
 vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'Find recent file' })
+vim.keymap.set('n', '<leader>fs', ':w<CR>', { desc = 'Save file' })
 vim.keymap.set('n', '<leader>fR', '', { desc = 'Rename file' })
 vim.keymap.set('n', '<leader>fD', '', { desc = 'Delete file' })
